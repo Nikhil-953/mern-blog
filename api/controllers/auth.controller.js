28a1e104088d7af3082e12428bccb387a6295dc4
Nikhil <nikhilanandraj94680@gmail.com>
2025-03-18 05:20:07 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
@@ -22,6 +23,33 @@ export const signup = async (req, res) => {
         message: `E11000 duplicate key error collection: Duplicate entry for ${JSON.stringify(error.keyValue)}`
       });
     }
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email == '' || password == '') {
+    return res.status(400).json({ success: false, message: "All fields are required" });
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return res.status(400).json({ success: false, message: "Invalid email" });
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return res.status(400).json({ success: false, message: "Invalid password" });
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const { password: pass, ...rest } = validUser._doc;
+
+    res.status(200).cookie( 'access_token', token, {
+      httpOnly: true}).json(rest);
+  } catch (error) {
     res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 };
