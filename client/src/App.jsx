b31ac2df82120fb2fb78e32b09c980d89622ded5
Nikhil <nikhilanandraj94680@gmail.com>
2025-03-20 02:53:52 +0530

    make dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import Projects from "./pages/Projects";
 import Header from "./components/Header";
 import React from "react";
 import "flowbite/dist/flowbite.min.css";
-
+import PrivateRoute from './components/PrivateRoute';
 
 
 
@@ -24,7 +24,9 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<PrivateRoute/>}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>  
         <Route path="/projects" element={<Projects />} />
       </Routes>
        <Footer />
